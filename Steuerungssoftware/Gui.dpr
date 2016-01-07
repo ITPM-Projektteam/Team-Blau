12da5ebbbea6b4aa29163e35fbad2b0d1bf3c897
F4lsm34ns <jv072409@LABSYSOPT-PC12.fhad.fh-muenster.de>
2016-01-07 13:45:48 +0100

    Überarbeitet mit Svens Kommentaren + Ordner verschieben

---
+++ Steuerungssoftware/Gui.dpr
@@ -0,0 +1,44 @@
+//******************************************************************************
+{*
+ * @file Gui.exe
+ * @author Jonah
+ * @date 04.01.2016
+ * @brief Benutzeroberfläche
+ * @copyright Copyright © 2015 ITPM-Projektteam
+ *
+ * @license
+ *
+ *  Diese Datei ist Teil von ***.
+ *
+ *  *** ist Freie Software: Sie können es unter den Bedingungen
+ *  der GNU General Public License, wie von der Free Software Foundation,
+ *  Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ *  veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ *  *** wird in der Hoffnung, dass es nützlich sein wird, aber
+ *  OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ *  Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ *  Siehe die GNU General Public License für weitere Details.
+ *
+ *  Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ *  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *}
+//******************************************************************************
+
+program Gui_exe;
+
+uses
+  Vcl.Forms,
+  Gui_Form1 in 'Gui_Form1.pas' {Form1},
+  Gui_From2 in 'Gui_From2.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+
+end.
