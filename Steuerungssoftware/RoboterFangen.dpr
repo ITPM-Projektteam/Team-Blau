5605c3c00200e4b07f5411c3a0f2d3879c1a173b
Obi8 <ez953761@LabSysOpt-PC9.fhad.fh-muenster.de>
2016-02-05 13:59:19 +0100

    Eine Unit für Konstanten hinzugefügt
    Konstanten:
    -Mindestabstand
    -Spielfeldbreite
    -Spielfeldhoehe
    -Nullvektor

--- Steuerungssoftware/RoboterFangen.dpr
+++ Steuerungssoftware/RoboterFangen.dpr
@@ -10,7 +10,8 @@ uses
   mHauptformular in 'mHauptformular.pas' {Hauptformular},
   mTKI in 'mTKI.pas',
   ClientUndServer in 'ClientUndServer.pas',
-  Client in 'Client.pas';
+  Client in 'Client.pas',
+  mKonstanten in 'mKonstanten.pas';
 
 {$R *.res}
 
---
+++ Steuerungssoftware/mKonstanten.pas
@@ -0,0 +1,15 @@
+unit mKonstanten;
+
+interface
+
+uses mVektor;
+
+const MINDESTABSTAND = 0.2;
+      SPIELFELDBREITE = 3;
+      SPIELFELDHOEHE = 4;
+
+      NULLVEKTOR: TVektor = (x: 0; y:0);
+
+implementation
+
+end.
